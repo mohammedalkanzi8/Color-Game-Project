3af08ce5e7ce4f436f8544792fbd30a1fc41fff2
Mohamed Kanzi <mohammedalkanzi8@gmail.com>
2018-10-09 02:25:55 +0300

    Adding the javascript logic

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ var colors = [
 	"rgb(255, 0, 255)",
 ]
 
-var squares = document.querySelectorAll(".square")
-pickedColor = colors[3];
-colorDisplay = document.getElementById("colorDisplay")
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -21,10 +23,25 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.background;
 		// Compare the color with the picked color
 		if (clickedColor === pickedColor) {
-			alert("True");
+			messageDisplay.textContent = "Correct";
+			changeColor(clickedColor);
 		} else {
-			alert("Wrong")
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 
 	});
+}
+// All the squares match the correct color
+function changeColor(color) {
+	// Loop through colors
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = color;
+		
+	}
+}
+// Random function to pick a color 
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
